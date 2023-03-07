ead9145f48ab9281dfe1948b779a8b92ae96751e
bilal-codora <bhu@codora.io>
2023-03-07 16:58:44 +0500

    q

--- second/urls.py
+++ second/urls.py
@@ -3,7 +3,5 @@ from django.urls import path, include
 from second import views
 
 urlpatterns = [
-   
     path('', views.index, name='second')
-    
 ]
--- second/views.py
+++ second/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
 def index(request):
-    return HttpResponse('this is homw page')
+    return HttpResponse('this is home page')
